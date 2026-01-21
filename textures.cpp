51c69ea42658896300a349e66a206cfcd623942a
AnisDjerrab <anisdjerrab@gmail.com>
2026-01-21 21:12:00 +0100

    work in progress...

--- textures.cpp
+++ textures.cpp
@@ -2,6 +2,9 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "libs/SOIL2.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <cmath>
 #include <fstream>
 
@@ -18,38 +21,41 @@ int width, height;
 float aspect;
 
 void setupVertices(void) {
-    float vertexPositions[108] = {
-        -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, -0.5f,
-        0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f,
-        0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
-        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
-        -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, 0.5f,
-        -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f,
-        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f,
-        -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f,
+    float vertexPositions[54] = { -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+        1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 
+        1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f
     };
+    float TexCoords[36] = {
+    0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f,
+    0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f,
+    0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+    0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f,
+    0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f,
+    1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f };
     glGenVertexArrays(1, vao);
     glBindVertexArray(vao[0]);
     glGenBuffers(numVBOs, vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(TexCoords), TexCoords, GL_STATIC_DRAW);
 }
 
 GLuint createShaderProgram() {
     // read the shader files
     string vShaderSource = "";
     string fShaderSource = "";
-    ifstream vertFile("shaders/vertCube.glsl", ios::in);
+    ifstream vertFile("shaders/vertTexture.glsl", ios::in);
     string line = "";
     while (!vertFile.eof()) {
         getline(vertFile, line);
         vShaderSource.append(line + "\n");
     }
     vertFile.close();
-    ifstream fragFile("shaders/fragCube.glsl", ios::in);
+    ifstream fragFile("shaders/fragTexture.glsl", ios::in);
     while (!fragFile.eof()) {
         getline(fragFile, line);
         fShaderSource.append(line + "\n");
@@ -83,11 +89,24 @@ void display(GLFWwindow* window, double currentTime) {
     glUseProgram(renderingProgram);
     glfwGetFramebufferSize(window, &width, &height);
     aspect = (float)width / (float)height;
+    auto mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    auto projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+    auto mvMat = glm::mat4(1.0f);
+    mvMat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 2.0f, -5.0f));
+    auto pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(1);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureID);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
-    glBindVertexArray(vao[0]);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glDrawArrays(GL_TRIANGLES, 0, 18);
 }
 
 int main() {
